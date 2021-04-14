099e9123e840cc7b43e33ffc063313c5bca913b7
Kunjan Dalal <kunjee17@gmail.com>
2021-04-14 13:17:55 +0530

    minor refactor

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -15,7 +15,7 @@ import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1
   },
@@ -75,174 +75,10 @@ const styles = theme => ({
       display: "none"
     }
   }
-});
+}));
 
-class ToolbarComponent extends React.Component {
-  state = {
-    achorEl: false,
-    MobileMoreAnchorEl: false
-  };
-
-  handleProfileMenuOpen = event => {
-    this.setState({
-      achorEl: event.currentTarget
-    });
-  };
-
-  handleMobileMenuClose = () => {
-    this.setState({
-      mobileMoreAnchorEl: null
-    });
-  };
-
-  handleMenuClose = () => {
-    this.setState({
-      achorEl: null,
-      mobileMoreAnchorEl: null
-    });
-  };
-
-  handleMobileMenuOpen = event => {
-    this.setState({
-      mobileMoreAnchorEl: event.currentTarget
-    });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const isMenuOpen = Boolean(this.state.anchorEl);
-    const isMobileMenuOpen = Boolean(this.state.mobileMoreAnchorEl);
-
-    const menuId = "primary-search-account-menu";
-    const renderMenu = (
-      <Menu
-        anchorEl={this.state.anchorEl}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        id={menuId}
-        keepMounted
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        open={isMenuOpen}
-        onClose={this.handleMenuClose}
-      >
-        <MenuItem onClick={this.handleMenuClose}>Profile</MenuItem>
-        <MenuItem onClick={this.handleMenuClose}>My account</MenuItem>
-      </Menu>
-    );
-
-    const mobileMenuId = "primary-search-account-menu-mobile";
-    const renderMobileMenu = (
-      <Menu
-        anchorEl={this.state.mobileMoreAnchorEl}
-        anchorOrigin={{ vertical: "top", horizontal: "right" }}
-        id={mobileMenuId}
-        keepMounted
-        transformOrigin={{ vertical: "top", horizontal: "right" }}
-        open={isMobileMenuOpen}
-        onClose={this.handleMobileMenuClose}
-      >
-        <MenuItem>
-          <IconButton aria-label="show 4 new mails" color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-          <p>Messages</p>
-        </MenuItem>
-        <MenuItem>
-          <IconButton aria-label="show 11 new notifications" color="inherit">
-            <Badge badgeContent={11} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <p>Notifications</p>
-        </MenuItem>
-        <MenuItem onClick={this.handleProfileMenuOpen}>
-          <IconButton
-            aria-label="account of current user"
-            aria-controls="primary-search-account-menu"
-            aria-haspopup="true"
-            color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <p>Profile</p>
-        </MenuItem>
-      </Menu>
-    );
-
-    return (
-      <div className={classes.grow}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="open drawer"
-              onClick={this.props.openDrawerHandler}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" noWrap>
-              Covid-Help
-            </Typography>
-            {/* <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-                inputProps={{ "aria-label": "search" }}
-              />
-            </div> */}
-            <div className={classes.grow} />
-            {/* <div className={classes.sectionDesktop}>
-              <IconButton aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={this.handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </div>
-            <div className={classes.sectionMobile}>
-              <IconButton
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={this.handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </div> */}
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-      </div>
-    );
-  }
+const ToolbarComponent = () => {
+  return <div>Toolbar component</div>
 }
 
-export default withStyles(styles)(ToolbarComponent);
+export default ToolbarComponent;
