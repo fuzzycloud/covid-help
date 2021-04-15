888895932ad02cbeda892600f249e958f4a0e369
Kunjan Dalal <kunjee17@gmail.com>
2021-04-15 10:19:35 +0530

    contact us page added

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,8 +13,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
     marginTop: theme.spacing(50),
     padding: `${theme.spacing(6)}px 0`,
-
-  }
+  },
 }));
 
 // Use useStyles hooks
@@ -24,16 +23,13 @@ const Footer = () => {
   return (
     <footer className={classes.footer}>
       <Paper className={classes.root} elevation={1}>
-        <Typography component="p">
-          Created by Fuzzy Cloud
-        </Typography>
+        <Typography component="p">Created by Fuzzy Cloud</Typography>
         <Typography component="p">
           © {new Date().getFullYear()} All right reserved
         </Typography>
       </Paper>
     </footer>
-
   );
-}
+};
 
 export default Footer;
