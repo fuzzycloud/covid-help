116da3e189c07e4d608366208505b9dccdb06446
Deep.sodha <deep.sodha@fuzzycloud.com>
2021-04-15 16:58:50 +0530

    added

--- data/tiffin_service.ts
+++ data/tiffin_service.ts
@@ -167,4 +167,24 @@ export const tiffin_service_data: Info[] = [
       },
     ],
   },
+  {
+    name: 'patidar sweets and catress',
+    details:
+    'Free tiffin service from 10th April.  Please register name and address before 9 AM in morning and 5 PM in evening',
+    address: {
+      city: 'Surat',
+      state: 'Gujarat',
+      country: 'India',
+    },
+    contacts: [
+      {
+        name: 'kishorbhai trapasiya',
+        mobileNo: '+91-9377784894',
+      },
+      {
+        name: 'maganbhai koladiya',
+        mobileNo: '+91-9824110048',
+      },
+    ],
+  },
 ];
