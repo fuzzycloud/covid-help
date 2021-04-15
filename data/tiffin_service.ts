52006e7995d41e2aeac924ba6f34ab539f3a7b57
Deep.sodha <deep.sodha@fuzzycloud.com>
2021-04-15 14:21:32 +0530

    added data

--- data/tiffin_service.ts
+++ data/tiffin_service.ts
@@ -25,4 +25,146 @@ export const tiffin_service_data: Info[] = [
       },
     ],
   },
+
+  {
+    name: 'Bhavnagar Jain swetambar murti  - jayukaka tanawala ',
+    details:
+      'Free tiffin service from 9th March. if anyone interested to donate in this fund contact our head office',
+    address: {
+      city: 'Bhavnagar',
+      state: 'Gujarat',
+      country: 'India',
+    },
+    contacts: [
+      {
+        name: 'office',
+        mobileNo: '0278-2427384',
+      },
+      {
+        name: 'jayukaka tanawala',
+        mobileNo: '+91-9825205366',
+      },
+      {
+        name: 'sanjaybhai thar',
+        mobileNo: '+91-9427 558 072',
+      },
+      {
+        name: 'pareshbhai shah',
+        mobileNo: '+91-9377 525 151',
+      },
+      {
+        name: 'rushabhbhai shah',
+        mobileNo: '+91-98251 73742',
+      },
+      {
+        name: 'kepibhai',
+        mobileNo: '+91-9825474962',
+      },
+    ],
+  },
+
+  {
+    name: 'Ramvadi ',
+    details:
+      'Free tiffin service from 9th April.  Please register name and address before 9 AM in morning and 5 PM in evening',
+    address: {
+      city: 'Sihor',
+      state: 'Gujarat',
+      country: 'India',
+    },
+    contacts: [
+      {
+        name: 'office',
+        mobileNo: '02846-2420406',
+      },
+      {
+        name: 'office',
+        mobileNo: '02846-2515706',
+      },
+      {
+        name: 'prabhakarbhai vyas',
+        mobileNo: '+91-9925574380',
+      },
+      {
+        name: 'kuntal trivedi',
+        mobileNo: '+91-9879557075',
+      },
+    ],
+  },
+  {
+    name: 'Gurukrupa foundation - Kantaben kntilal Shah',
+    details:
+      'only for jain cast and available areas are naranpura, sola road, ghatlodiya , nirnaynagar , gota , bopal , cetelite',
+    address: {
+      city: 'Ahmedabad',
+      state: 'Gujarat',
+      country: 'India',
+    },
+    contacts: [
+      {
+        name: 'gurukrupa foundation',
+        mobileNo: '+91-9558354715',
+      },
+      {
+        name: 'gaurang mehta',
+        mobileNo: '+91-9898424360',
+      },
+      {
+        name: 'bhavesh jodhani',
+        mobileNo: '+91-9824322334',
+      },
+      {
+        name: 'manish doshi',
+        mobileNo: '+91-9558046263',
+      },
+      {
+        name: 'nilesh gandhi',
+        mobileNo: '+91-8980816551',
+      },
+      {
+        name: 'bimal chokjawala',
+        mobileNo: '+91-9925033059',
+      },
+      {
+        name: 'jatin khandhar',
+        mobileNo: '+91-9825610655',
+      },
+      {
+        name: 'sanjay shah',
+        mobileNo: '+91-9825334559',
+      },
+      {
+        name: 'pinkal shah',
+        mobileNo: '+91-9825013563',
+      },
+      {
+        name: 'nikunj shah',
+        mobileNo: '+91-8320148554',
+      },
+    ],
+  },
+  {
+    name: 'hotel skyland',
+    details:
+      'home food available only in 100 rupees free home delivery for quarantine people within 4 km',
+    address: {
+      city: 'Ahmedabad',
+      state: 'Gujarat',
+      country: 'India',
+    },
+    contacts: [
+      {
+        name: 'office',
+        mobileNo: '+91-27172 30429',
+      },
+      {
+        name: 'office',
+        mobileNo: '+91-98250 19941',
+      },
+      {
+        name: 'office',
+        mobileNo: '+91-79845 54612',
+      },
+    ],
+  },
 ];
