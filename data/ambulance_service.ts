c167511e53d84a4613fb42fc357a93f9cecbd68e
bhattami <bhattami21@gmail.com>
2021-04-16 11:21:56 +0530

    data for ambulance

--- data/ambulance_service.ts
+++ data/ambulance_service.ts
@@ -1,3 +1,26 @@
 import { Info } from '../types/info';
 
-export const ambulance_service_data: Info[] = [];
+export const ambulance_service_data: Info[] = [
+    {
+        name: 'Free Ambulance Service',
+        details:
+          'Private Ambulance Service during Curfew timings Only Ahmadabad city. NOTE - The patient must have doctor confirmation in advance.',
+        address: {
+          city: 'Ahmadabad',
+          state: 'Gujarat',
+          country: 'India',
+        },
+        contacts: [
+          {
+            name: 'Ravi vyas',
+            mobileNo: '+91-9624668228',
+          },
+          
+          {
+            name: 'Krisha travels',
+            mobileNo: '+91-8980825506',
+          },
+        ],
+      },
+    
+];
