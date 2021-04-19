1b22c413b680778f9ef62fa6a9a611c100c3b272
bhattami <bhattami21@gmail.com>
2021-04-19 14:18:04 +0530

    issue 42

--- data/beds_service.ts
+++ data/beds_service.ts
@@ -1,3 +1,6 @@
 import { Info } from '../types/info';
+import { beds_gujarat_rajkot_data } from './beds/gujarat/rajkot';
 
-export const beds_service_data: Info[] = [];
+export const beds_service_data: Info[] = [
+    ...beds_gujarat_rajkot_data
+];
--- data/oxygen_service.ts
+++ data/oxygen_service.ts
@@ -1,8 +1,10 @@
 import { Info } from '../types/info';
 import { oxygen_gujarat_bhavnagar_data } from './oxygen/gujarat/bhavnagar';
+import { oxygen_gujarat_rajkot_data } from './oxygen/gujarat/rajkot';
 import { oxygen_up_lacknow_data } from './oxygen/uttar_pradesh/lucknow';
 
 export const oxygen_service_data: Info[] = [
     ...oxygen_up_lacknow_data,
-    ...oxygen_gujarat_bhavnagar_data
+    ...oxygen_gujarat_bhavnagar_data,
+    ...oxygen_gujarat_rajkot_data
 ];
