5d5de61df53a68aa1826a336dac9cdbd33c90766
Kunjan Dalal <kunjee17@gmail.com>
2021-04-20 23:45:41 +0530

    api added for tiffins

--- pages/drs/[country]/[state]/[city].tsx
+++ pages/drs/[country]/[state]/[city].tsx
@@ -8,7 +8,6 @@ export const ServiceList = ({
   data,
   params,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  console.log(params);
   if (data) {
     return <InfoView data={data} />;
   }
--- pages/tiffins/index.tsx
+++ pages/tiffins/index.tsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
-import { InferGetStaticPropsType } from 'next';
-import { tiffin_service_data } from '../../data/tiffin_service';
-import { infoToSlimAddresses } from '../../types/info';
+import useSWR from 'swr';
 import CitySelection from '../../components/citySelection';
+import { SlimAddress } from '../../types/info';
+import { CircularProgress } from '@material-ui/core';
 
-const Home = ({
-  addresses,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return <CitySelection baseUrl={'tiffins'} addresses={addresses} />;
+const Home = () => {
+  const { data, error } = useSWR<SlimAddress[]>('/api/tiffins');
+  if (error) {
+    return <div>Faild to load data</div>;
+  }
+  if (!data) {
+    return <CircularProgress />;
+  }
+  return <CitySelection baseUrl={'tiffins'} addresses={data} />;
 };
 
-export const getStaticProps = async () => {
-  return {
-    props: {
-      addresses: infoToSlimAddresses(tiffin_service_data),
-    },
-  };
-};
+// export const getStaticProps = async () => {
+//   return {
+//     props: {
+//       addresses: infoToSlimAddresses(tiffin_service_data),
+//     },
+//   };
+// };
 
 export default Home;
