41d09fa4851fbe8b6cc6f6eb673374e7ed5fae41
Kunjan Dalal <kunjee17@gmail.com>
2021-04-21 12:11:17 +0530

    slim bug resolved

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Link from 'next/link';
 import Typography from '@material-ui/core/Typography';
