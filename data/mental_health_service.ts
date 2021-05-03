815bbead8019e29ea5fd78f363fad733121646d4
amibhatt <bhattami21@gmail.com>
2021-05-03 13:42:04 +0530

    project with ionic angular

--- data/mental_health_service.ts
+++
@@ -1,3 +0,0 @@
-import { Info } from '../types/info';
-
-export const mental_health_service_data: Info[] = [];
--- pages/api/services/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { slimServiceRoutes } from '../../../data/routes';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await slimServiceRoutes();
-  res
-    .status(200)
-    .json(data);
-};
